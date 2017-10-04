0cd6fba3ec09f4649ca479a5c6b6211ee7e6d8a3
Lihikoka <dainelcofa@gmail.com>
2017-10-04 13:18:08 +0800

    Use smaller entry

--- phonebook_opt.h
+++ phonebook_opt.h
@@ -5,8 +5,8 @@
 
 /* TODO: After modifying the original version, uncomment the following
  * line to set OPT properly */
-// #define OPT 1
-typedef struct __PHONE_BOOK_ENTRY {
+#define OPT 1
+typedef struct __PHONE_BOOK_DETAIL {
     char lastName[MAX_LAST_NAME_SIZE];
     char firstName[16];
     char email[16];
@@ -18,6 +18,12 @@ typedef struct __PHONE_BOOK_ENTRY {
     char state[2];
     char zip[5];
     struct __PHONE_BOOK_ENTRY *pNext;
+} phonebook_detail;
+
+typedef struct __LAST_NAME_ENTRY {
+    char lastName[MAX_LAST_NAME_SIZE];
+    phonebook_detail *detail;
+    struct __LAST_NAME_ENTRY *pNext;
 } entry;
 
 entry *findName(char lastName[], entry *pHead);
